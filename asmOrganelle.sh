fb1539022ba6799278b3c695518394a5d4db13f8
Diego M Riano-Pachon <diriano@gmail.com>
2018-04-11 15:12:41 -0300

    Moving user-defined vars to external file

--- asmOrganelle.sh
+++ asmOrganelle.sh
@@ -3,30 +3,7 @@
 #=Check required Software=
 source ./asmOrganelle_reqSoft.sh
 #=Global Variables=
-# Must be defined by the user
-
-# Number of cores
-NSLOTS=10
-
-# Number of MIRABAIT runs
-NUMBER_ITER_BAIT=5
-
-# Number of kmers that must match to keep a hit
-NUMBER_KMERS=10
-
-# Number of Racon runs within each Mirabait run
-NUMBER_ITER_CNS=3
-
-# Dataset (fasta) containing all long reads
-ALL_READS=../all.Vintermedia.fasta
-#ALL_READS=bait_Vintermedia_ChloroplastMaizeRice.MIRABAITiter2.fasta
-
-# Prefix for output files
-PREFIX=Vintermedia_ChloroplastMaizeRice
-
-# Fasta database with collection of  organelle genomes used as reference in the first bait iteration
-EXTERNAL_REFERENCE=../References/Maize_Rice_plastid.fasta
-
+source ./asmOrganelle_config.sh
 
 ##############################################
 ##############################################
